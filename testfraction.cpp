1241696676e862ed10cf682b9219c0dbbb17acfa
Mubariz Yolchiyev <139426979+prplguyy@users.noreply.github.com>
2024-11-27 09:10:29 +0100

    testfraction.cpp

--- testfraction.cpp
+++ testfraction.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include "fraction.h"
 
@@ -7,7 +5,25 @@ using namespace std;
 
 int main(void)
 {
-	// create fractions
 	Fraction a, b;
+
+    cin >> a;
+    cin >> b;
+
+    Fraction product = a * b;
+    Fraction quotient = a / b;
+    Fraction sum = a + b;
+    Fraction difference = a - b;
+
+    cout << sum << endl;
+    cout << difference << endl;
+    cout << product << endl;
+    cout << quotient << endl;
+    
+    cout << (a>b) << endl;
+    cout << (a<b) << endl;
+
+    return 0;
+}    
 
 
