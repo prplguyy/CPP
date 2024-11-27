e7f38966fd127f965c2f58ad1c863a613bfc660f
Mubariz Yolchiyev <139426979+prplguyy@users.noreply.github.com>
2024-11-27 09:10:13 +0100

    fraction.h

--- fraction.h
+++ fraction.h
@@ -7,15 +7,27 @@ private:						// internal implementation is hidden
 	int num;					// numerator
 	int den;					// denominator
 
-	int gcd(int a, int b);		// calculates the gcd of a and b
-	int lcm(int a, int b);
+	int gcd(int a, int b) const;		// calculates the gcd of a and b
+	int lcm(int a, int b) const;
 
 public:
 	Fraction();					// empty constructor
 	Fraction(int, int = 1); 	// constructor taking values for fractions and
 								// integers. Denominator by default is 1
-	void print();				// prints it to the screen
+								
+	
+    friend std::ostream& operator<<(std::ostream& os, const Fraction& f);
+    friend std::istream& operator>>(std::istream& is, Fraction& f);
+    Fraction operator*(const Fraction& f) const;
+    Fraction operator/(const Fraction& f) const;
+	Fraction operator+(const Fraction& f) const;
+	Fraction operator-(const Fraction& f) const;
 
+	Fraction& operator=(const Fraction& f);
+
+	bool operator<(const Fraction &f) const;
+	bool operator>(const Fraction &f) const;
+    
 };
 
 
